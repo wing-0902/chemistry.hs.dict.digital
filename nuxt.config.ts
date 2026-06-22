df5eb6a0cb0bc870cb807791c8b2845fb92b1db7
Wing <114337707+wing-0902@users.noreply.github.com>
2026-06-22 16:54:42 +0900

    Update site title and URL for chemistry

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   app: {
     head: {
-      title: '地理探求辞書',
+      title: '化学 - dict.digital',
       htmlAttrs: {
         lang: 'ja'
       }
@@ -41,8 +41,8 @@ export default defineNuxtConfig({
   },
   modules: ['@nuxtjs/sitemap', '@nuxt/content', '@unocss/nuxt'],
   site: {
-    url: 'https://geography.hs.dict.digital',
-    name: '地理探求',
+    url: 'https://chemistry.hs.dict.digital',
+    name: '化学 - dict.digital',
     trailingSlash: true
   },
   vite: {
